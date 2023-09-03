61545d3d4635871a55c2c96e2ffcf668c8844ba6
Piyush Jain <piyushjain5350@gmail.com>
2023-09-03 19:51:37 +0530

    1 dicitionary commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Routes , Route } from 'react-router-dom';
+import Header from './Components/Header';
+import MainPage from './Components/MainPage';
+import History from './Components/History';
+import WordMeaning from './Components/WordDetails';
 
 function App() {
+  document.title='Dictonary App';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen App">
+      <Header/>
+      <Routes>
+        <Route path="/" element = {<MainPage/>}/>
+        <Route path="/history" element = {<History/>}/>
+        <Route path="/word/:word" component={<WordMeaning/>} /> {/* Dynamic route */}
+      </Routes>
     </div>
   );
 }
